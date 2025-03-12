484b5bddd7179d7b234efc3b1d283a26b43ca693
Ochir00 <mongolbucker@gmail.com>
2025-03-12 13:40:54 +0800

    test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,31 +1,6 @@
-import {
-  ContextMenu,
-  ContextMenuCheckboxItem,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
-  ContextMenuSeparator,
-  ContextMenuShortcut,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu";
 
 export default function ContextMenuDemo() {
   return (
-    <ContextMenu>
-      <ContextMenuTrigger className="">
-        <button className="flex items-center h-[36px] text-black px-2 rounded-full border-[#EF4444] border-[1px]">
-        <p>123</p>
-        </button>
-      </ContextMenuTrigger>
-      <ContextMenuContent className="w-64">
-        <ContextMenuItem inset>Edit</ContextMenuItem>
-        <ContextMenuItem inset>Delete</ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <div></div>
   );
 }
